4ff19704494d78b1bc100fc953113432706c320a
owunmiv <wunmieeplus@gmail.com>
2025-10-14 20:47:43 +0100

    Initial Commit

---
+++ CV Portfolio/CV_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CV_PortfolioApp.swift
+//  CV Portfolio
+//
+//  Created by Omowunmi on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct CV_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CV Portfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CV Portfolio
+//
+//  Created by Omowunmi on 14/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
